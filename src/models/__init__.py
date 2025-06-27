24a6d2d459772080da58f0bc8dfc0c74490985ea
Filippo Gombac <filippo.gombac@studbocconi.it>
2025-06-27 12:31:45 +0200

    Remove deprecated predictor classes from model exports to streamline architecture

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,16 +3,10 @@ from .base.vit import ViT
 from .base.cnn import CNNEncoder
 from .base.mlp import MLPEncoder
 from .mask_encoder import MaskEncoder
-from .selection_mask_predictor import SelectionMaskPredictor
-from .reward_predictor import RewardPredictor
-from .continuation_predictor import ContinuationPredictor
 
 __all__ = [
     "ViT",
     "CNNEncoder",
     "MLPEncoder",
     "MaskEncoder",
-    "SelectionMaskPredictor",
-    "RewardPredictor",
-    "ContinuationPredictor",
 ]
